a10ee3b45374e75d219de92b1ddf49e9df2beda4
jlassig <las22004@byui.edu>
2023-06-24 11:31:04 -0600

    error handling again

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,13 +1,23 @@
 const errorController = {}
 errorController.generateError = function (req, res, next) {
-  try {
-    ///for getting the error:
-    const error = new Error("Internal Server Error")
-    error.status = 500
-    throw error
-  } catch (error) {
+  try{
+    console.log(variableNameThatDoesntExist)
+  }catch(error){
+    error.status = 500;
+    console.error(error.status)
     next(error)
   }
 }
 
 module.exports = errorController
+
+
+  // try {
+
+  //   ///for getting the error:
+  //   const error = new Error("Internal Server Error")
+  //   error.status = 500
+  //   throw error
+  // } catch (error) {
+  //   next(error)
+  // }
