2b6a05f16c2d7f7db680f42a3eccd8ee722b8680
jlassig <las22004@byui.edu>
2023-07-03 20:13:07 -0600

    wk5 assign-almost done with task 1

---
+++ public/js/loggedin.js
@@ -0,0 +1,8 @@
+console.log("inside loggedin")
+
+const logoutLink = document.querySelector("#logoutLink")
+const welcomeLink = document.querySelector("#welcomeLink")
+accountLink = document.querySelector("#accountLink")
+logoutLink.style.display = "block";
+welcomeLink.style.display = "block"
+accountLink.style.display = "none"
---
+++ public/js/loggedout.js
@@ -0,0 +1,8 @@
+console.log("inside loggedout")
+
+const logoutLink = document.querySelector("#logoutLink")
+const welcomeLink = document.querySelector("#welcomeLink")
+const accountLink = document.querySelector("#accountLink")
+logoutLink.style.display = "none"
+welcomeLink.style.display = "none"
+accountLink.style.display = "block"
